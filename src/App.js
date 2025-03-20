a17267c4605feea71053f92dc35da90b0b0c9601
aadhar101 <aadharchand01@gmail.com>
2025-03-20 08:55:07 +0545

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
